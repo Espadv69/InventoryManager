a6c3709bbdcdc67d6140c5bcdc35c78c453065b9
Espadv69 <aitorjob69@gmail.com>
2025-02-14 18:01:55 +0100

    Add Product Model

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,1 +1,8 @@
 const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  description: { type: String, required: true },
+  stock: { type: Number, default: 0 },
+})
