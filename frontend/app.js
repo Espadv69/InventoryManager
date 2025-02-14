d252d647e72dc3b5c3bd70f661763d230c5cb0d5
Espadv69 <aitorjob69@gmail.com>
2025-02-14 19:30:26 +0100

    Add DOM elements and form.preventDefault to not reload the page when listener a submit

---
+++ frontend/app.js
@@ -0,0 +1,9 @@
+document.querySelector('.addProduct-form').addEventListener('submit', async function(event) {
+  event.preventDefault()
+
+  // DOM elements
+  const $inputName = document.querySelector('name')
+  const $inputPrice = document.querySelector('.price')
+  const $inputDescription = document.querySelector('.description')
+  const $inputStock = document.querySelector('.stock')
+})
