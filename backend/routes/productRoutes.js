d5d6a2888a4b2010745ec41ca24885b3a46617c3
Espadv69 <aitorjob69@gmail.com>
2025-02-14 18:17:31 +0100

    Add router post to create and add new products

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -13,4 +13,15 @@ router.get('/', async (req, res) => {
   }
 })
 
+router.post('/', async (req, res) => {
+  try {
+    const { name, price, description, stock } = req.body
+    const newProduct = new Product({ name, price, description, stock })
+    await newProduct.save()
+    res.status(201).json(newProduct)
+  } catch(err) {
+    res.status(400).json({ message: 'Error creating product:', err})
+  }
+})
+
 module.exports = router
