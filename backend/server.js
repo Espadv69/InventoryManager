1bf573085c5f244f3f37046cd910aecadc27dbbe
Espadv69 <aitorjob69@gmail.com>
2025-02-14 17:39:18 +0100

    Add dotenv config, create app with express and add cors middleware

--- backend/server.js
+++ backend/server.js
@@ -2,3 +2,9 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const dotenv = require('dotenv')
+
+dotenv.config()
+
+const app = express()
+app.use(express.json())
+app.use(cors())
