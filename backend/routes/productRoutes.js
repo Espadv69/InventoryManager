7107ada6dc53f2cdb68458fc3d89976d75673f6c
Espadv69 <aitorjob69@gmail.com>
2025-02-14 18:06:57 +0100

    Add route to get all products

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,2 +1,14 @@
 const express = require('express')
 const Product = require('../models/Product')
+
+const router = express.Router()
+
+// Get all products
+router.get('/', async (req, res) => {
+  try {
+    const products = await Product.find()
+    res.json(products)
+  } catch (err) {
+    res.status(500).json({ message: 'Error fetching products', err })
+  }
+})
