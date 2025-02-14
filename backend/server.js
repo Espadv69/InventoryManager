fc606cd825bf62cb32f8f617eaad02fc990f3e7f
Espadv69 <aitorjob69@gmail.com>
2025-02-14 17:41:07 +0100

    Add PORT constant and mongoose connection

--- backend/server.js
+++ backend/server.js
@@ -8,3 +8,10 @@ dotenv.config()
 const app = express()
 app.use(express.json())
 app.use(cors())
+
+const PORT = process.env.PORT || 5000
+
+mongoose
+  .connect('http://127.0.0.1:27017/products')
+  .then(() => console.log('MongoDB connected ✅'))
+  .catch((err) => console.error('Error connecting to MongoDB:', err))
