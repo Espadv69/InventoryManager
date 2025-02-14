31b1858df71e42a3974abfc9ae2a54564473a0d6
Espadv69 <aitorjob69@gmail.com>
2025-02-14 19:32:01 +0100

    Add input values into variable to better handly

--- frontend/app.js
+++ frontend/app.js
@@ -6,4 +6,9 @@ document.querySelector('.addProduct-form').addEventListener('submit', async func
   const $inputPrice = document.querySelector('.price')
   const $inputDescription = document.querySelector('.description')
   const $inputStock = document.querySelector('.stock')
+
+  const name = $inputName.value
+  const price = $inputPrice.value
+  const description = $inputDescription.value
+  const stock = $inputStock.value
 })
