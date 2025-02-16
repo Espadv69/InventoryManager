e1e4e58988f2002674946536b9d4816d0076a782
Espadv69 <aitorjob69@gmail.com>
2025-02-16 13:33:10 +0100

    Add product creation and update routes with error handling

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -13,15 +13,35 @@ router.get('/', async (req, res) => {
   }
 })
 
+// Create a new product
 router.post('/', async (req, res) => {
   try {
     const { name, price, description, stock } = req.body
     const newProduct = new Product({ name, price, description, stock })
     await newProduct.save()
     res.status(201).json(newProduct)
-  } catch(err) {
-    res.status(400).json({ message: 'Error creating product:', err})
+  } catch (err) {
+    res.status(400).json({ message: 'Error creating product:', err })
   }
 })
 
+// Update a product by ID
+router.put('/:id', async (req, res) => {
+  try {
+    const { name, price, description, stock } = req.body
+    const updateProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      { name, price, description, stock },
+      { new: true, runValidators: true }
+    )
+
+    if (!updateProduct) {
+      return res.status(404).json({ message: 'Product not found' })
+    }
+
+    res.json(updateProduct)
+  } catch (err) {
+    res.status(400).json({ message: 'Error updating product', err })
+  }
+})
 module.exports = router
